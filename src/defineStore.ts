e0c226c1ee54e5d0808bc4334757120d41342e14
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-07-21 04:13:25 +0000

    support useStore in `actions`

--- src/defineStore.ts
+++ src/defineStore.ts
@@ -1,53 +1,114 @@
-import { useEffect, useState } from "react"
+import {
+  useEffect,
+  useId,
+  useState,
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+} from "react"
 
 import { computed } from "./reactivity/computed"
-import { cleanupEffect, ReactiveEffect } from "./reactivity/effect"
-import type {
-  DeepReadonly,
-  UnwrapNestedRefs
-} from "./reactivity/reactive"
 import {
-  reactive
-} from "./reactivity/reactive"
+  cleanupEffect,
+  effectNotActived,
+  ReactiveEffect
+} from "./reactivity/effect"
+import type { DeepReadonly, UnwrapNestedRefs } from "./reactivity/reactive"
+import { reactive } from "./reactivity/reactive"
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type FlatGetters<Getters = {}> = {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  readonly // @ts-expect-error
-  [Name in keyof Getters]: ReturnType<Getters[Name]>;
-};
+  readonly // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  [Name in keyof Getters]: ReturnType<Getters[Name]>
+}
 
 export interface OptionsStore<State, Actions, Getters, ReadonlyState> {
-  state: () => State
+  id?: string
+  state?: () => State
   actions?: Actions & ThisType<State & Actions & FlatGetters<Getters>>
   getters?: Getters & ThisType<State & Actions & FlatGetters<Getters>>
   readonly?: ReadonlyState
 }
 
-export type UsedStore<State, Actions, Getters, ReadonlyState extends boolean> = [
+export type UsedStore<
+  State,
+  Actions,
+  Getters,
+  ReadonlyState extends boolean
+> = [
   (ReadonlyState extends true
     ? DeepReadonly<UnwrapNestedRefs<State>>
     : UnwrapNestedRefs<State>) &
     Actions &
     FlatGetters<Getters>,
   Actions & ThisType<State & Actions & FlatGetters<Getters>>
-];
+]
 
-export function defineStore<
-  State extends Record<string, unknown>,
+const randomUUID = (): string => {
+  return Math.random().toString(36).slice(2, 9)
+}
+
+function defineStore<
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  State extends Record<string, unknown> = {},
   // eslint-disable-next-line @typescript-eslint/ban-types
   Actions = {},
-  Getters extends Record<string, (
+  Getters extends Record<
+    string,
+    (
       state: UnwrapNestedRefs<State> & Actions & FlatGetters<Getters>
-    // eslint-disable-next-line @typescript-eslint/ban-types, @typescript-eslint/no-explicit-any
-    ) => any> = {},
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ) => any
+    // eslint-disable-next-line @typescript-eslint/ban-types
+  > = {},
+  ReadonlyState extends boolean = false
+>(
+  id: string,
+  options: OptionsStore<State, Actions, Getters, ReadonlyState>
+): () => UsedStore<State, Actions, Getters, ReadonlyState>
+// eslint-disable-next-line no-redeclare
+function defineStore<
+  State extends Record<string, unknown> = {},
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Actions = {},
+  Getters extends Record<
+    string,
+    (
+      state: UnwrapNestedRefs<State> & Actions & FlatGetters<Getters>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ) => any
+    // eslint-disable-next-line @typescript-eslint/ban-types
+  > = {},
   ReadonlyState extends boolean = false
 >(
   options: OptionsStore<State, Actions, Getters, ReadonlyState>
-): () => UsedStore<State, Actions, Getters, ReadonlyState> {
+): () => UsedStore<State, Actions, Getters, ReadonlyState>
+
+// eslint-disable-next-line no-redeclare
+function defineStore<
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  State extends Record<string, unknown> = {},
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  Actions = {},
+  Getters extends Record<
+    string,
+    (
+      state: UnwrapNestedRefs<State> & Actions & FlatGetters<Getters>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ) => any
+    // eslint-disable-next-line @typescript-eslint/ban-types
+  > = {},
+  ReadonlyState extends boolean = false
+>(
+  id: string | OptionsStore<State, Actions, Getters, ReadonlyState>,
+  options?: OptionsStore<State, Actions, Getters, ReadonlyState>
+): (used?: boolean) => UsedStore<State, Actions, Getters, ReadonlyState> {
+  if (!options) {
+    options = id as Exclude<typeof id, string>
+    id = options.id ?? randomUUID()
+  }
   const { actions = {} as Actions } = options
 
-  const state = reactive(options.state()) as (ReadonlyState extends true
+  const state = reactive(options.state?.() ?? {}) as (ReadonlyState extends true
     ? DeepReadonly<UnwrapNestedRefs<State>>
     : UnwrapNestedRefs<State>) &
     Actions &
@@ -81,21 +142,29 @@ export function defineStore<
       )) as FlatGetters<Getters>
   )
 
-  return (): UsedStore<State, Actions, Getters, ReadonlyState> => {
-    // eslint-disable-next-line @typescript-eslint/ban-types
-    const [, setUnique] = useState(undefined as unknown as {})
-    const effect = new ReactiveEffect(() => {
-      setUnique({})
-      console.log("force update")
-      cleanupEffect(effect)
-      // eslint-disable-next-line functional/immutable-data
-      effect.active = false
-      effect.stop()
-      // clear effect after call one fn
-    })
-    effect.record()
-    useEffect(effect.end.bind(effect))
+  return (used): UsedStore<State, Actions, Getters, ReadonlyState> => {
+    try {
+      if (used || effectNotActived(id as string)) return [state, actions]
 
+      // eslint-disable-next-line @typescript-eslint/ban-types
+      const [, setUnique] = useState(undefined as unknown as {})
+      const effect = new ReactiveEffect(
+        () => {
+          setUnique({})
+          cleanupEffect(effect)
+          // eslint-disable-next-line functional/immutable-data
+          effect.active = false
+          effect.stop()
+          // clear effect after call one fn
+        },
+        null,
+        id as string
+      )
+      effect.record()
+      useEffect(effect.end.bind(effect))
+    } catch {}
     return [state, actions]
   }
 }
+
+export { defineStore }
